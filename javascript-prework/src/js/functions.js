ca56c385a28ebf01e7afa64e58cf0f7f022bcd6f
sebgancarz <seb.gancarz@gmail.com>
2019-03-23 01:14:30 +0100

    Add main.js, Modified functions.js, Delete script.js files

--- javascript-prework/src/js/functions.js
+++ javascript-prework/src/js/functions.js
@@ -1,9 +1,42 @@
-function printMessage(msg) {
-  const div = document.createElement('div');
-  div.innerHTML = msg;
-  document.getElementById('messages').appendChild(div);
+// Player choice
+function playerChoice() {
+  game.playerHand = this.dataset.option;
+  hands.forEach(hand => hand.style.borderColor = '');
+  this.style.borderColor = 'orangered';
+  const playerImage = document.querySelector('.player-image');
+  playerImage.src = this.src;
 }
 
-function clearMessages() {
-  document.getElementById('messages').innerHTML = '';
+// Computer choice
+function computerChoice() {
+  const compHand = hands[Math.floor(Math.random() * 3)];
+  const compImage = document.querySelector('.comp-image');
+  compImage.src = compHand.src;
+  return compHand.dataset.option;
 }
+
+// Check result
+function checkResult(player, comp) {
+  if ((player === "paper" && comp === "rock") || (player === "rock" && comp === "scissors") || (player === "scissors" && comp === "paper")) {
+    return 'win';
+  } else if (player === comp) {
+    return 'draw';
+  } else {
+    return 'loss';
+  }
+}
+
+// Init function
+function startGame() {
+  if (!game.playerHand) {
+    return alert('Wybierz dłoń!');
+  }
+  game.computerHand = computerChoice();
+  const roundResult = checkResult(game.playerHand, game.computerHand);
+  console.log(game.playerHand, game.computerHand, roundResult);
+
+}
+
+hands.forEach(hand => hand.addEventListener('click', playerChoice));
+
+document.querySelector('.play').addEventListener('click', startGame)
