b3ec304793a21f1a6a810c9616b772806f7e5066
sebgancarz <seb.gancarz@gmail.com>
2019-03-28 22:27:13 +0100

    Add class removing to handler function

--- 06_javascript-blog/src/js/script.js
+++ 06_javascript-blog/src/js/script.js
@@ -2,21 +2,28 @@
 
 const links = document.querySelectorAll('.titles a');
 
-function titleClickHandler(e) {
+function titleClickHandler(event) {
+  console.log(this);
 
-  /* remove class 'active' from all article links  */
+  /* [DONE] remove class 'active' from all article links  */
+  const activeLinks = document.querySelectorAll('titles a.active');
+  for (let activeLink of activeLinks) {
+    activeLink.classList.remove('active');
+  }
+  /* [IN PROGRESS] add class 'active' to the clicked link */
 
-  /* add class 'active' to the clicked link */
 
-  /* remove class 'active' from all articles */
-
+  /* [DONE] remove class 'active' from all articles */
+  const activeArticles = document.querySelectorAll('.post');
+  for (let activeArticle of activeArticles) {
+    activeArticle.classList.remove('active');
+  }
   /* get 'href' attribute from the clicked link */
 
   /* find the correct article using the selector (value of 'href' attribute) */
 
   /* add class 'active' to the correct article */
 
-  console.log(e);
 }
 
 for (let link of links) {
