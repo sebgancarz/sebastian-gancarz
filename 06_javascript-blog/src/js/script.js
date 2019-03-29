8f496c8005adb97e95bacdd1714aee3a14e23eab
sebgancarz <seb.gancarz@gmail.com>
2019-03-29 23:41:33 +0100

    Modified comments

--- 06_javascript-blog/src/js/script.js
+++ 06_javascript-blog/src/js/script.js
@@ -4,16 +4,16 @@ const links = document.querySelectorAll('.titles a');
 
 function titleClickHandler(e) {
   e.preventDefault();
-  /* [DONE] remove class 'active' from all article links  */
+  /* remove class 'active' from all article links  */
   const activeLinks = document.querySelectorAll('titles a.active');
   for (let activeLink of activeLinks) {
     activeLink.classList.remove('active');
   }
-  /* [DONE] add class 'active' to the clicked link */
+  /* add class 'active' to the clicked link */
   const clickedElement = this;
   clickedElement.classList.add('active');
 
-  /* [DONE] remove class 'active' from all articles */
+  /* remove class 'active' from all articles */
   const activeArticles = document.querySelectorAll('.post');
   for (let activeArticle of activeArticles) {
     activeArticle.classList.remove('active');
