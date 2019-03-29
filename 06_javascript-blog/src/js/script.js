c9fb80a678255d42bd408d1b0be10fdeb02e40f6
sebgancarz <seb.gancarz@gmail.com>
2019-03-29 23:32:41 +0100

    Select and add class to targeted article

--- 06_javascript-blog/src/js/script.js
+++ 06_javascript-blog/src/js/script.js
@@ -19,12 +19,11 @@ function titleClickHandler(e) {
     activeArticle.classList.remove('active');
   }
   /* get 'href' attribute from the clicked link */
-
-
+  const articleSelector = clickedElement.getAttribute('href');
   /* find the correct article using the selector (value of 'href' attribute) */
-
+  const targetArticle = document.querySelector(articleSelector);
   /* add class 'active' to the correct article */
-
+  targetArticle.classList.add('active');
 }
 
 for (let link of links) {
