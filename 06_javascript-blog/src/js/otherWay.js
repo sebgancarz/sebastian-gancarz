3ec64b1002aa2277dd32a5fd79ce5f71ad601426
sebgancarz <seb.gancarz@gmail.com>
2019-04-01 19:46:18 +0200

    Add ESlint and dependencies, package.json modified

--- 06_javascript-blog/src/js/otherWay.js
+++ 06_javascript-blog/src/js/otherWay.js
@@ -1,5 +1,5 @@
 function titleClickHandler(e) {
-  e.preventDefault()
+  e.preventDefault();
   const activeLinks = document.querySelectorAll('titles a.active');
   /* remove class 'active' from all article links  */
   activeLinks.forEach(activeLink => activeLink.classList.remove('active'));
@@ -15,9 +15,9 @@ function titleClickHandler(e) {
   /* add class 'active' to the correct article */
   targetArticle.classList.add('active');
 }
-const optArticleSelector = '.post',
-  optTitleSelector = '.post-title',
-  optTitleListSelector = '.titles';
+const optArticleSelector = '.post';
+const optTitleSelector = '.post-title';
+const optTitleListSelector = '.titles';
 
 function generateTitleLinks() {
   /* Remove contents of titlelist */
@@ -27,20 +27,20 @@ function generateTitleLinks() {
   const articles = document.querySelectorAll(optArticleSelector);
   let html = '';
   /* For each article */
-  articles.forEach(article => {
+  articles.forEach((article) => {
     /* Get the article id */
     const articleId = article.getAttribute('id');
     /* Find the title element and get the title from it */
     const articleTitle = article.querySelector(optTitleSelector).innerHTML;
     /* Create HTML of the link */
-    const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
+    const linkHTML = `<li><a href="#${articleId}"><span>${articleTitle}</span></a></li>`;
     /* Insert link into titleList */
     html += linkHTML;
-  })
+  });
   titleList.innerHTML = html;
 }
 
-generateTitleLinks()
+generateTitleLinks();
 
 const links = document.querySelectorAll('.titles a');
 
