3cc38e7a406388542057d44873782557e5811634
sebgancarz <seb.gancarz@gmail.com>
2019-05-04 12:13:31 +0200

    Split all Classes into separate files

---
+++ 07_project-pizzeria/src/js/app.js
@@ -0,0 +1,64 @@
+import {
+  Product
+} from './components/Product.js';
+import {
+  Cart
+} from './components/Cart.js';
+import {
+  select,
+  settings,
+  classNames,
+  templates
+} from './settings.js';
+
+const app = {
+  initData: function () {
+    const thisApp = this;
+    thisApp.data = {};
+    const url = settings.db.url + '/' + settings.db.product;
+
+    fetch(url)
+      .then(function (rawResponse) {
+        return rawResponse.json();
+      })
+      .then(function (parsedResponse) {
+        console.log('parsedResponse', parsedResponse);
+        thisApp.data.products = parsedResponse;
+        thisApp.initMenu();
+      });
+  },
+  initMenu: function () {
+    const thisApp = this;
+
+    for (let productData in thisApp.data.products) {
+      new Product(thisApp.data.products[productData].id, thisApp.data.products[productData]);
+    }
+  },
+
+  initCart: function () {
+    const thisApp = this;
+
+    const cartElem = document.querySelector(select.containerOf.cart);
+    thisApp.cart = new Cart(cartElem);
+
+    thisApp.productList = document.querySelector(select.containerOf.menu);
+
+    thisApp.productList.addEventListener('add-to-cart', function (event) {
+      app.cart.add(event.detail.product);
+    });
+  },
+
+  init() {
+    const thisApp = this;
+    console.log('*** App starting ***');
+    console.log('thisApp:', thisApp);
+    console.log('classNames:', classNames);
+    console.log('settings:', settings);
+    console.log('templates:', templates);
+
+    thisApp.initData();
+    thisApp.initCart();
+  },
+};
+
+app.init();
